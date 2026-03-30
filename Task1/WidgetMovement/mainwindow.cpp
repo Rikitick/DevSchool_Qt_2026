6fee9a1a95546a00d45000c2330151a475545b53
Rikitick <rick.tick15@mail.ru>
2026-03-30 23:14:56 +0500

    add (solution): add mainwindow.cpp

--- Task1/WidgetMovement/mainwindow.cpp
+++ Task1/WidgetMovement/mainwindow.cpp
@@ -1,5 +1,6 @@
-#include <QTimer>
 #include <QPushButton>
+#include <QTimer>
+#include <QPropertyAnimation>
 
 #include "mainwindow.h"
 
@@ -7,8 +8,36 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     resize(300, 600);
-/// Your code here...
+    m_timer = new QTimer(this);
+    connect(m_timer, &QTimer::timeout, this, [this](){
+        QPushButton* btn = new QPushButton("*", this);
+        btn->resize(40, 40);
+        btn->show();
 
+        int randX = std::rand() % (this->width() - btn->width());
+        int randY = std::rand() % 100;
+        int randDuration = 5000 + (std::rand() % 5000);
+
+        QPropertyAnimation* anim = new QPropertyAnimation(btn, "pos", this);
+        anim->setDuration(randDuration);
+        anim->setStartValue(QPoint(randX, randY));
+        anim->setEndValue(QPoint(randX, this->height()));
+        connect(btn, &QPushButton::clicked, this, [btn, anim](){
+            anim->stop();
+            btn->deleteLater();
+            anim->deleteLater();
+        });
+        connect(anim, &QPropertyAnimation::finished, this, [btn, anim, this](){
+            this->setWindowTitle("you LOSE!!!");
+            this->setStyleSheet("QMainWindow { background-color: red; }");
+            btn->deleteLater();
+            anim->deleteLater();
+        });
+
+        anim->start();
+    });
+    int appearanceTime = 100 + (std::rand() % 900);
+    m_timer->start(appearanceTime);
 }
 
 MainWindow::~MainWindow()
